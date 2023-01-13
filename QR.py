94a8e2b848fac06232214afdece7c9a764cafbf7
davidnene <davienesh4@gmail.com>
2023-01-13 11:29:04 +0300

    Clean code

--- QR.py
+++ QR.py
@@ -4,10 +4,8 @@ import png
 from PIL import Image
 
 
-link = input("Enter link to generate QR code:")
-title = input("Enter link title here:")
-
 def qr_generator(link, title):
     qr_code = pyqrcode.create(link)
     qr_code.png(f"{title}.png", scale=5)
-    Image.open(f"{title}.png")
+    im = Image.open(f"{title}.png")
+    im.show()
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
+"""
+We will use this file to test the program
+""" 
 
+#import the function from QR.py
 qr_generator = __import__('QR').qr_generator
 
+#intilialize input to capture the detils in the CLI
 link = input("Enter link to generate QR code:")
 title = input("Enter link title here:")
 
+#initialize the function
 qr_generator(link, title)
